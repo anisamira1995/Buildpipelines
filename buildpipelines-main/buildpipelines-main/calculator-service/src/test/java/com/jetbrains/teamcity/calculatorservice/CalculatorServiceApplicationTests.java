bfceecd35d6c2b84172e8e75bb55878188ee93b2
Anisamira <36921366+anisamira1995@users.noreply.github.com>
2022-08-15 15:09:06 +0800

    Add files via upload

---
+++ buildpipelines-main/buildpipelines-main/calculator-service/src/test/java/com/jetbrains/teamcity/calculatorservice/CalculatorServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jetbrains.teamcity.calculatorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	void contextLoads2() {
+	}
+}
